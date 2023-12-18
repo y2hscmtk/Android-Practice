4fccfc11517423263ec52db0e486ff3080ec99d2
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-19 00:02:27 +0900

    ADD : 커스텀 라디오 버튼 & 체크박스 예제 코드

--- app/src/main/java/com/example/androidpractice/Custom/CustomRadioButtonActivity.kt
+++ app/src/main/java/com/example/androidpractice/Custom/CustomRadioButtonActivity.kt
@@ -2,11 +2,27 @@ package com.example.androidpractice.Custom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import androidx.core.content.ContextCompat
 import com.example.androidpractice.R
 
 class CustomRadioButtonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_radio_button)
+
+        val greenColor = ContextCompat.getColor(this,R.color.Green3)
+        val blackColor = ContextCompat.getColor(this,R.color.black)
+        val likeBtn = findViewById<CheckBox>(R.id.cbx_like)
+
+        likeBtn.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                likeBtn.text = (likeBtn.text.toString().toInt() + 1).toString()
+                likeBtn.setTextColor(greenColor)
+            } else {
+                likeBtn.text = (likeBtn.text.toString().toInt() - 1).toString()
+                likeBtn.setTextColor(blackColor)
+            }
+        }
     }
 }

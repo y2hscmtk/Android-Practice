2437f36fc72c63a3841e0e1f83ad29302c38da30
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-27 10:22:09 +0900

    ADD : DrawerActivity - Hambuger Menu

---
+++ app/src/main/java/com/example/androidpractice/drawer/DrawerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidpractice.drawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidpractice.R
+
+class DrawerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer)
+    }
+}

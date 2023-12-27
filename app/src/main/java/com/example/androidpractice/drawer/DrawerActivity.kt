a4ec8ae1252ac81857cec01297f88d0243982530
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-27 11:15:49 +0900

    FEAT : Drawer 추가(햄버거 메뉴)

--- app/src/main/java/com/example/androidpractice/drawer/DrawerActivity.kt
+++ app/src/main/java/com/example/androidpractice/drawer/DrawerActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidpractice.drawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.ImageButton
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.androidpractice.R
 
 
@@ -10,12 +13,21 @@ import com.example.androidpractice.R
 // Navigation Drawer : 평소에는 숨겨져있다가 사용자의 엑션을 통해 화면에 뷰가 드러남(슬라이드 형식)
 // 툴바 구현을 위해 style.xml에서 AppTheme style의 parent를 "Theme.AppCompat.Light.NoActionBar"로 바꾸어 주어야 합
 
-// 레이아웃의 가장 바깥 부분을 DrawerLayout 로 감싸고 사용자에게 보여줄 화면은 NavigationView 에 작성한다.
-// NavigationView 에 보여줄 레이아웃은 별도로 작성하며, NavigationView의 headerLayout으로 작성하여 포함시킨다.
+// 레이아웃의 가장 바깥 부분을 DrawerLayout로 감싸고, 버튼을 눌러서 사용자에게 Drawer 화면을 보여준다.
 
 class DrawerActivity : AppCompatActivity() {
+
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drawer)
+
+        drawer = findViewById(R.id.drawer)
+
+        val btnShowDrawer = findViewById<ImageButton>(R.id.btn_show_drawer)
+        btnShowDrawer.setOnClickListener {
+            drawer.openDrawer(Gravity.RIGHT)
+        }
     }
 }
